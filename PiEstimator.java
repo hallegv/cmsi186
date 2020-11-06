c25596c8ae2b3dc7f0c623a39da6a9750b8b9f09
hallegv <56059233+hallegv@users.noreply.github.com>
2020-11-05 16:35:20 -0800

    Fixed indentation

--- PiEstimator.java
+++ PiEstimator.java
@@ -6,25 +6,25 @@ public class PiEstimator {
                 throw new IllegalArgumentException("Exactly one argument required");
             }
             int darts = Integer.parseInt(args[0]);
-          System.out.println(estimate(darts));
+            System.out.println(estimate(darts));
         } catch (NumberFormatException e) {
-              System.err.println("Argument must be an integer");
-            }
-         catch (IllegalArgumentException e) {
+            System.err.println("Argument must be an integer");
+        } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
     }
 
     public static double estimate(int darts) {
-      if (darts < 1){
-        throw new IllegalArgumentException("At least one dart required");
-      }
+        if (darts < 1){
+            throw new IllegalArgumentException("At least one dart required");
+        }
+        
         double dartsThrown = 0;
 
         for (var i = 0; i <= darts; i++){
-          if (Math.hypot(Math.random(), Math.random())<= 1){
-            dartsThrown++;
-          }
+            if (Math.hypot(Math.random(), Math.random())<= 1){
+                dartsThrown++;
+            }
         }
         return 4 * ((double)dartsThrown) / darts;
     }
