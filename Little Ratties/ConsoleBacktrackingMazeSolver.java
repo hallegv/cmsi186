3442d00e37f91fa4f01f606c46283ec539ee3a92
hallegv <56059233+hallegv@users.noreply.github.com>
2020-11-11 18:25:07 -0800

    Update ConsoleBacktrackingMazeSolver.java

--- Little Ratties/ConsoleBacktrackingMazeSolver.java
+++ Little Ratties/ConsoleBacktrackingMazeSolver.java
@@ -15,7 +15,7 @@ public class ConsoleBacktrackingMazeSolver {
         public void mazeChanged(Maze maze) {
             try {
                 Thread.sleep(200);
-            } catch (InterruptedException ignored) {
+            } catch (InterruptedException ignored) { 
             }
             System.out.println(CLEAR_SCREEN + CURSOR_HOME + maze);
             System.out.println();
@@ -27,8 +27,7 @@ public class ConsoleBacktrackingMazeSolver {
             if (args.length > 1) {
                 throw new IllegalArgumentException("Program requires 0 or 1 arguments");
             }
-            var maze = (args.length == 1) ? Maze.fromFile(args[0])
-                    : Maze.fromScanner(new Scanner(System.in));
+            var maze = (args.length == 1) ? Maze.fromFile(args[0]) : Maze.fromScanner(new Scanner(System.in));
             var success = new BacktrackingMazeSolver().solve(maze, listener);
             System.out.println(success ? "Made it! " : "Poor ratty!");
         } catch (Exception e) {
