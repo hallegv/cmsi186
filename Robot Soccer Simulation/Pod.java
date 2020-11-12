230ae3bd4521826bf999c9fd73928bd97eb3a0cd
hallegv <56059233+hallegv@users.noreply.github.com>
2020-11-11 16:13:39 -0800

    Create Pod.java

---
+++ Robot Soccer Simulation/Pod.java
@@ -0,0 +1,32 @@
+import java.util.function.Function;
+
+public class Pod {
+    private String name;
+    private Function<Double, Double> velocityFunction;
+
+    public Pod(String name, Function<Double, Double> velocityFunction) {
+        this.name = name;
+        this.velocityFunction = velocityFunction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double v(double t) {
+        return velocityFunction.apply(t);
+    }
+
+    public double distanceTraveled(double startTime, double endTime, double slices) {
+      if (slices < 1) {
+        throw new IllegalArgumentException("At least one slice required");
+      }
+      var distance = 0.0;
+      var timeSlice = (endTime - startTime) / slices;
+      for (var i = 0; i < slices; i++) {
+        var t = startTime + i * timeSlice;
+        distance += v(t) * timeSlice;
+      }
+      return distance;
+    }
+}
